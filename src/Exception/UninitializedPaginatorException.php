66086fd43a8c83537f09c6e6d96df6f9fb17d125
Takashi Kanemoto <ttskch@gmail.com>
2024-02-06 13:05:43 +0900

    refactor: 💡 fix cs

--- src/Exception/UninitializedPaginatorException.php
+++ src/Exception/UninitializedPaginatorException.php
@@ -8,7 +8,7 @@ use Ttskch\PaginatorBundle\Paginator;
 
 class UninitializedPaginatorException extends \LogicException
 {
-    public function __construct(int $code = 0, \Throwable $previous = null)
+    public function __construct(int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct(sprintf('The class "%s" must not be used before initialization.', Paginator::class), $code, $previous);
     }
